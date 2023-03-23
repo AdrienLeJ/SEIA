6859fc7108bd2877af7644aa8016587bdb5cd54e
AlexCaroff <alexandre.caroff@isen-ouest.yncrea.fr>
2023-03-23 16:02:54 +0100

    IHM

--- listenning_function.py
+++ listenning_function.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QDialog
 
 
 import cv2
+import sys, psutil
 
 
 class Function(QDialog):
@@ -9,8 +10,21 @@ class Function(QDialog):
         super(Function,self).__init__()
         self.ONOFF.clicked.connect(self.onClicked)
     def onClicked(self):
-        cv2.VideoCapture(1)
+
+        ### retour caméra ###
+        cam = cv2.VideoCapture(1)
+        while(cam.isOpened()):
+            ret, frame = cam.read()
+            if ret == True:
+                self.displayimage(frame,1)
+                cv2.waitKey()
+
+         ### affichage orientation en ° d'après prédiction ###
+
+
+        ### coloration des directions suivant la direction prise ###
+
+
         self.ONOFF.clicked.connect(self.offClicked)
     def offClicked(self):
         cv2.VideoCapture(0)
-
--- main.py
+++ main.py
@@ -6,35 +6,24 @@ Created on Wed Mar 22 14:26:36 2023
 """
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
-
+from IHMv2 import Ui_MainWindow
 from listenning_function import Function
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super (MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
 
-        # Load the user interface from the .ui file
-        loader = QUiLoader()
-        ui_file = QFile("IHM.ui")
-        ui_file.open(QFile.ReadOnly)
-        self.ui = loader.load(ui_file)
-        ui_file.close()
-
-        # Set up the user interface
-        self.setCentralWidget(self.ui.centralwidget)
-        self.ui.pushButton.clicked.connect(self.handle_button_click)
 
         self.inner_function = Function()
 
-    def handle_button_click(self):
-        print("Button clicked!")
-
-
-
-if __name__ == "__main__":
+if __name__== '__main__':
     app = QApplication(sys.argv)
+
     window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    window. show()
+
+    sys. exit(app.exec_())
+
+
